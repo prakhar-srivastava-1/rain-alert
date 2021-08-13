8c5a384d0f4efe706b2b5790c9f5103cbaebccca
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2021-08-13 23:49:29 +0530

    bring together all components in main.py

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 from data import WeatherData
 from message_client import MessageClient
+from rain import will_it_rain
 
+weather_data = WeatherData()
+weather_codes = weather_data.weather_data
+print(weather_codes)
 
-# weather_data = WeatherData()
-# weather_codes = weather_data.weather_data
-# print(weather_codes)
-#
-#
-# # check if its raining
-# for code in weather_codes:
-#     if code < 700:
-#         print("It's raining")
-#         break
-
-message_client = MessageClient()
-message_client.send_alert()
+if will_it_rain(weather_codes):
+    message_client = MessageClient()
+    message_client.send_alert()
