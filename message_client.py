0aea5d23dcd2f9bdd56732cbb6f77bf3d56f3238
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2021-08-14 00:01:06 +0530

    add http proxy client for hosted code

--- message_client.py
+++ message_client.py
@@ -1,5 +1,7 @@
+import os
 from secrets import TWILIO_PHONE, TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, MY_PHONE
 from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
 
 
 class MessageClient:
@@ -9,9 +11,13 @@ class MessageClient:
         self.phone = TWILIO_PHONE
         self.auth_token = TWILIO_AUTH_TOKEN
         self.my_phone = MY_PHONE
-        self.client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+        proxy_client = TwilioHttpClient()
+        proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+
+        self.client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, http_client=proxy_client)
 
     def send_alert(self):
+
         message = self.client.messages.create(
             body="It is going to rain today. Remember to bring an umbrella.",
             from_=self.phone,
